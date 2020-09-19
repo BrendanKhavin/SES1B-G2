65a4ccef4a8d5d0bb746dcb5d93b07bfcc1a920a
Aung Soe Kyaw <aungsoekyaw2017@gmail.com>
2020-09-19 16:12:59 +1000

    Lunch Menu page is working
    -displaying Menu data from the database

--- app/src/main/java/com/example/erestaurant/LunchMenu.java
+++ app/src/main/java/com/example/erestaurant/LunchMenu.java
@@ -26,8 +26,10 @@ public class LunchMenu extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+
     // private FirebaseDatabase fBase;
     private DatabaseReference reffy;
+    private DatabaseReference reffy1;
     long count;
     //DatabaseReference reff2;
     FirebaseDatabase db;
@@ -39,7 +41,7 @@ public class LunchMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_menu);
-        /*
+
         //current use null check
         bAuth = FirebaseAuth.getInstance();
         currentUserId = bAuth.getCurrentUser().getUid();
@@ -47,8 +49,9 @@ public class LunchMenu extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(),Login.class));
             finish();
         }
-        */
-        reffy = FirebaseDatabase.getInstance().getReference().child("meals").child("Lunch");
+
+
+        /*
         ArrayList<meals> meallist = new ArrayList<>();
         meallist.add(new meals("curry","23"));
         meallist.add(new meals("soda","11"));
@@ -59,7 +62,12 @@ public class LunchMenu extends AppCompatActivity {
         meallist.add(new meals("curry","23"));
         meallist.add(new meals("soda","11"));
         meallist.add(new meals("salad","12"));
-
+        meallist.add(new meals("curry","23"));
+        meallist.add(new meals("soda","11"));
+        meallist.add(new meals("salad","12"));
+        meallist.add(new meals("curry","23"));
+        meallist.add(new meals("soda","11"));
+        meallist.add(new meals("salad","12"));
 
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
@@ -67,23 +75,64 @@ public class LunchMenu extends AppCompatActivity {
         mAdapter = new CardAdapter(meallist);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-        /*
-        //get the count value for the lunch meals data
+                        //get the count value for the lunch meals data
+                     count = dataSnapshot.getChildrenCount();
+                        //set lunch meal list
+                        ArrayList<meals> meallist = new ArrayList<>();
+                            for (long i = 1; i <= count; i++) {
+                                String foodname = dataSnapshot.child("foodName").getValue().toString();
+                                String foodprice = dataSnapshot.child("foodPrice").getValue().toString();
+                                meallist.add(new meals(foodname, foodprice));
+
+                                mRecyclerView = findViewById(R.id.recyclerView);
+                                mRecyclerView.setHasFixedSize(true);
+                                mLayoutManager = new LinearLayoutManager(LunchMenu.this);
+                                mAdapter = new CardAdapter(meallist);
+                                mRecyclerView.setLayoutManager(mLayoutManager);
+                                mRecyclerView.setAdapter(mAdapter);
+         */
+        final ArrayList<meals> meallist = new ArrayList<>();
+        reffy = FirebaseDatabase.getInstance().getReference().child("meals").child("Lunch");
         reffy.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 if(dataSnapshot.exists()) {
+                    //get the count value for the lunch meals data
                     count = dataSnapshot.getChildrenCount();
-                    //set lunch meal list
-                    ArrayList<meals> meallist = new ArrayList<>();
-                    for(long i = 0; i < count ; i++){
-                        String foodname = dataSnapshot.child("foodName").getValue().toString();
-                        String foodprice = dataSnapshot.child("foodPrice").getValue().toString();
-                        meallist.add(new meals(foodname,foodprice));
+                    for(int i = 1; i <= count; i++) {
+                        String mealinfo = "fMeal" + i;
+                        reffy1 = FirebaseDatabase.getInstance().getReference().child("meals").child("Lunch").child(mealinfo);
+                        reffy1.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                if(dataSnapshot.exists()) {
+
+                                    String foodname = dataSnapshot.child("foodName").getValue().toString();
+                                    String foodprice = dataSnapshot.child("foodPrice").getValue().toString();
+                                    meallist.add(new meals(foodname,"Price: $" + foodprice));
+
+                                    mRecyclerView = findViewById(R.id.recyclerView);
+                                    mRecyclerView.setHasFixedSize(true);
+                                    mLayoutManager = new LinearLayoutManager(LunchMenu.this);
+                                    mAdapter = new CardAdapter(meallist);
+                                    mRecyclerView.setLayoutManager(mLayoutManager);
+                                    mRecyclerView.setAdapter(mAdapter);
+                                } else {
+
+                                    Toast.makeText(getApplicationContext(), "****NOT FOUND****", Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                //Toast.makeText(this, databaseError.getCode(), Toast.LENGTH_SHORT.show();
+                                Toast.makeText(LunchMenu.this, "unable", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
-                }
-                else {
+                } else {
                     Toast.makeText(getApplicationContext(), "****NOT FOUND****", Toast.LENGTH_LONG).show();
                 }
             }
@@ -94,10 +143,6 @@ public class LunchMenu extends AppCompatActivity {
                 Toast.makeText(LunchMenu.this, "unable", Toast.LENGTH_SHORT).show();
             }
         });
-
-         */
-
-
 
     }
 
